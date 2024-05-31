4126259075d81565eaeab50e91deb7e421d95062
ankitfs <ankitagarwal.fs@gmail.com>
2024-05-31 17:07:16 +0530

    Minor Changes in ddl and dml files

--- sc-productmgmt/src/main/resources/sql/product_tables_dml.sql
+++ sc-productmgmt/src/main/resources/sql/product_tables_dml.sql
@@ -3,7 +3,7 @@
 insert into product_category  ("name",description, active, created_at, parent_id) values ('women','women', true, now(), null);
 insert into product_category  ("name",description, active, created_at, parent_id) values ('kurti','kurtis', true, now(), (select id from product_category where name='women'));
 
-insert into product ("name", description, sku, category_id, price, active, created_at) values ('choodi dar kurtis', 'choodi dar kurtis', 'PO1-101', 2, 110, true, now() )
+insert into product ("name", description, sku, category_id, price, active, created_at) values ('choodi dar kurtis', 'choodi dar kurtis', 'PO1-101', 2, 110, true, now() );
 
 insert into product_inventory ( quantity , created_at) values(10, now());
 
@@ -11,10 +11,7 @@ insert into product_inventory ( quantity , created_at) values(10, now());
 update product set inventory_id = 1, discount_id =1  where id=1;
 
 --25/4/24
-update product_category set level = 0 where id=1;
-update product_category set level = 1  where id=2;
+update product_category set category_level = 0 where id=1;
+update product_category set category_level = 1  where id=2;
 
 ------------------------------------------------------------------------------------------------
-
---admin_type table
-insert into admin_type (admin_type, permissions, created_at) values (1,'BOTH', now());
